ea640e2366923cb75b803fc24a35a45e4ba5ffd5
Shihab <Shihab@DESKTOP-J8T4NV6>
2017-08-27 18:53:19 +0600

    Layer and Entities Added

---
+++ V.Doc/V.Doc_Entity/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace V.Doc_Entity
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String UserName { get; set; }
+        public String Email { get; set; }
+        public DateTime Birthdate { get; set; }
+        public int Age { get; set; }
+        public String Password { get; set; }
+        public String Gender { get; set; }
+    }
+}
